47c582655143cea47f92ee6bbdc6f6bb7ffee518
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-29 16:32:30 +0200

    A Python function that inserts a new document in a collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Insert a new doc into the database"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a new school into the database"""
+    school = kwargs
+    _id = mongo_collection.insert_one(school)
+    return _id
